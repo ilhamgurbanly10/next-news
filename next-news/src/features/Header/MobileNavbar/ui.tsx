0bcc7c0ca0ead601abee1994c5fa8de397e10a56
ilhamgurbanly10 <Ilham_gurbanly@mail.ru>
2024-01-26 01:50:14 +0400

    c

--- next-news/src/features/Header/MobileNavbar/ui.tsx
+++ next-news/src/features/Header/MobileNavbar/ui.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { useTranslation } from 'next-i18next'
 import { Props } from './type';
 import Menu from '@/shared/ui/Menu';
 import { paths } from './data';
 
 export const UI: React.FC<Props> = ({ className })  => {
-
-  const { t } = useTranslation('common');
 
   return (
     <nav className={`mobile-navbar relative flex items-center ${className ? className : ''}`}>
